e2e444400d7a4fb2d49b04577f1074973c1f92b1
Nichol Fernando <115018248+Mr-Nichol@users.noreply.github>
2023-05-15 10:15:36 +0200

    add buttons, event listeners & hover effect

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
+const container = document.querySelector('.container');
+const buttons = document.querySelectorAll('.button');
 const grid = document.querySelector('.grid');
+const reset = document.querySelector('.reset');
 
-function createTheGrid(columns,rows){
-    for(c=0; c<columns; c++){
-        let columns = document.createElement('div');
-        columns.classList.add('columns')
-        grid.appendChild(columns);
-        for(r=0; r<rows; r++){
-            let rows = document.createElement('div');
-            rows.classList.add('rows')
-            columns.appendChild(rows);
-        }
+function amount (){
+    buttons.forEach((button) => {
+        button.addEventListener('click',function(){
+            grid.innerHTML = '';
+            createGrid(button.value,button.value);
+        })       
+    });
+}
+
+
+function createGrid(colums,rows){
+    let cellAmount = colums*rows;
+    
+    let cellSize = 500/rows
+    for (r=0;r<cellAmount; r++){
+        let cell = document.createElement('div');
+        cell.classList.add("cell");
+        cell.style.cssText = `height: ${cellSize}px; width: ${cellSize}px;`
+        cell.addEventListener('mouseover',
+        ()=> cell.style.cssText = `background-color: black;height: ${cellSize}px; width: ${cellSize}px;`);
+        grid.appendChild(cell);
     }
 }
 
-createTheGrid(16,16);
+createGrid(16,16);
+amount();
+reset.addEventListener('click',function(){
+    grid.innerHTML = ''
+    createGrid(16,16)
+})
